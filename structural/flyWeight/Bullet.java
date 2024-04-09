0a3278ae73db01f59dfca43a863cead33b80c5b3
Ayush <ayushagarwal.dev@gmail.com>
2024-04-09 22:40:01 +0530

    add FlyWeight pattern code

---
+++ structural/flyWeight/Bullet.java
@@ -0,0 +1,6 @@
+package structural.flyWeight;
+
+public class Bullet {
+    //    A separate class for storing sharable heavy-image which is same across all objects
+    private String image;
+}
---
+++ structural/flyWeight/FlyingBullet.java
@@ -0,0 +1,9 @@
+package structural.flyWeight;
+
+public class FlyingBullet {
+    //    class for flying-bullet category, having reference to base bullet class for sharable properties
+    private double x;
+    private double y;
+    private double radius;
+    private Bullet bullet;
+}
